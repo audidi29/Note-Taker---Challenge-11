33e9233468902d8db05bc9c1c10a60922a27f819
Your Name <you@example.com>
2023-10-16 16:49:30 -0700

    changing port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
